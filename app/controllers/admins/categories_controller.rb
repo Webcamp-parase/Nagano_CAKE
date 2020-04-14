6c12bb8e35b27a1244e993200e18f841bc33ae33
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-04-14 23:47:33 +0900

    Add routes(categories,products)

---
+++ app/controllers/admins/categories_controller.rb
@@ -0,0 +1,13 @@
+class Admins::CategoriesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admins do
+    resources :categories, only: [:index, :create, :edit, :update]
+    resources :products, only: [:index, :show, :new, :create, :edit, :update]
+  end
 end
