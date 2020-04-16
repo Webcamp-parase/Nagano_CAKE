0569d1b04cba6fbfaa2407281e843896d8d95c7f
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-04-16 17:50:54 +0900

    Add products/show,edit,update

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -4,6 +4,7 @@ class Admins::ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -11,19 +12,23 @@ class Admins::ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def create
     @product = Product.new(product_params)
     @product.save
-    redirect_to admins_products_path
+    redirect_to admins_product_path(@product.id)
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admins_product_path(params[:id])
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :non_tax_price, :image_id, :status)
+    params.require(:product).permit(:name, :description, :category_id, :non_tax_price, :image, :status)
   end
 
 end
--- app/helpers/admins/products_helper.rb
+++ app/helpers/admins/products_helper.rb
@@ -1,2 +1,7 @@
 module Admins::ProductsHelper
+
+	def addTax(non_tax_price)
+		taxed_price = (BigDecimal(non_tax_price) * BigDecimal("1.1")).ceil
+		"#{taxed_price.to_s(:delimited)}"
+	end
 end
