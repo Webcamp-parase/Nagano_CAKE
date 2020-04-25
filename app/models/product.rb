15cb1240da50216c2033f08bcb6e2a604dcff348
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-04-25 22:09:40 +0900

    Fix layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,11 @@ class Product < ApplicationRecord
 	belongs_to :category
 	has_many :cart_items, dependent: :destroy
 	has_many :order_products, dependent: :destroy
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :category_id, presence: true
+	validates :non_tax_price, presence: true
+	validates :image, presence: true
+	validates :status, presence: true
 end
